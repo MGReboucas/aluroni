b6f26839566b8851eb224f809b7a1e03291b2419
MGReboucas <reboucas444@gmail.com>
2025-01-30 23:57:55 -0300

    terminando curso e tudo funcionando

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import {createRoot } from 'react-dom/client';
 import 'normalize.css';
 import './index.css';
-import Router from './routes.js';
+import Router from './routes';
 
 const conteiner = document.getElementById('root');
 const root = createRoot(conteiner!);
--- src/pages/Inicio/index.tsx
+++ src/pages/Inicio/index.tsx
@@ -2,11 +2,18 @@ import Cardapio from '../../data/cardapio.json';
 import styles from './inicio.module.scss';
 import stylesTema from '../../styles/Tema.module.scss';
 import NossaCasa from '../../assets/nossa_casa.png';
-
+import { useNavigate } from 'react-router-dom';
+import { Prato } from 'types/Prato';
 
 export default function Inicio() {
      let PratosRecomendados = [...Cardapio];
      PratosRecomendados = PratosRecomendados.sort(() => 0.5 - Math.random()).splice(0, 3);
+     const navigate = useNavigate();
+
+     function RedirecionarParaDetalhes(prato: Prato){
+          navigate(`/prato/${prato.id}`, { state: { prato }, replace: true});
+     }
+
      return (
           <section>
                <h3 className={stylesTema.titulo}>
@@ -21,7 +28,10 @@ export default function Inicio() {
                               <div className={styles.recomendado__imagem}>
                                    <img src={item.photo} alt={item.title} />
                               </div>
-                              <button className={styles.recomendado__botao}>
+                              <button 
+                                   className={styles.recomendado__botao}
+                                   onClick={() => RedirecionarParaDetalhes(item)}
+                                   >
                                    Ver Mais
                               </button>
                          </div>
